3697fbb24425ddc35fd9e48db922b0910356c7f2
salocor <mechikids@gmail.com>
2023-01-15 21:18:26 -0500

    Set request to https rather than http

--- hider/main.js
+++ hider/main.js
@@ -7,7 +7,7 @@ var logLocation = () => {
     currLat = location.coords.latitude;
     currLong = location.coords.longitude;
     debug.innerHTML = "<h4>Latitude: " + currLat + "<br>Longitude: " + currLong + "</h3>";
-    
+
     //console.log(location.coords.accuracy);
   });
 };
@@ -16,7 +16,7 @@ var timer = setInterval(logLocation, 1000);
 
 const sendLocation = async()=>{
   let errorThrown = false;
-  var request = "http://" + document.getElementById('ip').value + ":5000/location?lat=" + currLat + "&long=" + currLong;
+  var request = "https://" + document.getElementById('ip').value + ":5000/location?lat=" + currLat + "&long=" + currLong;
   console.log("Sent request: " + request);
 
   try {
