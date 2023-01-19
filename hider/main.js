6999b89b24675d0c6a585a319aba05bb5c8a0663
Corban <corbanmech@protonmail.com>
2023-01-19 11:21:26 -0500

    here are your files back. now cooperate
    :

---
+++ hider/main.js
@@ -0,0 +1,40 @@
+var currLat;
+var currLong;
+
+
+var logLocation = () => {
+  navigator.geolocation.getCurrentPosition(function(location) {
+    currLat = location.coords.latitude;
+    currLong = location.coords.longitude;
+    console.log("Latitude: " + currLat);
+    console.log("Longitude: " + currLong);
+    sendLocation();
+    //console.log(location.coords.accuracy);
+  });
+};
+
+var timer = setInterval(logLocation, 1000);
+
+const sendLocation = async()=>{
+  let errorThrown = false;
+   var request = "https://salocor.pythonanywhere.com/location?lat=" + currLat + "&long=" + currLong;
+  debugResult.innerHTML = "<h4>Latitude: " + currLat + "<br>Longitude: " + currLong + "</h3>";
+  console.log("Sent request: " + request);
+
+  try {
+    const response =  fetch(request,
+      {
+        method: "GET",
+      }
+    );
+    if (response.ok) {
+      console.log("Sent ok");
+      debugResult.innerHTML = "<h3>Result: " + response + "</h3>";
+    }
+    }
+    catch(e) {
+      console.log("Error with request");
+
+  }
+}
+//
